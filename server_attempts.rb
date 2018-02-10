a7b7600e113c85f7ba34b2409c78267cc3859bd9
alexcutschall <alexander.cutschall@gmail.com>
2018-02-10 11:50:37 -0700

    Added an iteration that can receive HTTP requests but doesn't increment:

--- server_attempts.rb
+++ server_attempts.rb
@@ -1,8 +1,24 @@
 require 'socket'
+require 'pry'
 
-server = TCPServer.new(9292) #server bind to port 9292, or the mailbox
-loop do
-  listener = server.accept #wait for a client to connect
-  listener.puts "Hello World!"
-  listener.close
+tcp_server = TCPServer.new(9292)
+listener = tcp_server.accept
+
+server_requests = 0
+request_lines = []
+while line = listener.gets and !line.chomp.empty?
+  request_lines << line.chomp
 end
+
+output = "Hello World! (#{server_requests})"
+headers = ["http/1.1 200 ok",
+           "date: #{Time.now.strftime('%a, %e %b %Y %H:%M:%S %z')}",
+           "server: ruby",
+           "content-type: text/html; charset=iso-8859-1",
+           "content-length: #{output.length}\r\n\r\n"].join("\r\n")
+
+listener.puts headers
+listener.puts output
+
+listener.close
+puts "\nResponse complete, exiting."
