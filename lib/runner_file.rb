c1cfd8e1b298227604290096c9deecec7a4f8330
alexcutschall <alexander.cutschall@gmail.com>
2018-02-11 09:55:07 -0700

    Reformatted the beginning into a class Server with the responses to backtrack and make sure I understood how everything was working

--- lib/runner_file.rb
+++ lib/runner_file.rb
@@ -1,15 +1,37 @@
 require 'socket'
 require 'pry'
-require './lib/server'
 
-server = Server.new
-server.start
-server.response
-server.output
-server.headers
-server.puts headers
-server.puts output
+server = TCPServer.new(9292)
+client = server.accept
 
-puts "Wrote this response:"
+server_requests = 0
+while client = server.accept
+  server_requests += 1
+
+  request_lines = []
+  while line = client.gets and !line.chomp.empty?
+    request_lines << line.chomp
+  end
+
+  response = ["VERB: #{request_lines[0].split(" ")[0]}",
+              "Path: #{request_lines[0].split(" ")[1]}",
+              "Protocol: #{request_lines[0].split(" ")[2]}",
+              "Host: #{request_lines[1][6..14]}",
+              "Port: #{request_lines[1][-4..-1]}",
+              "Origin: #{request_lines[1][6..14]}",
+              "#{request_lines[6][0..7]}\n#{request_lines[6][8..-1]}"].join("\n")
+
+  output = "Hello World! (#{server_requests})\n#{response}"
+  headers = ["http/1.1 200 ok",
+             "date: #{Time.now.strftime('%a, %e %b %Y %H:%M:%S %z')}",
+             "server: ruby",
+             "content-type: text/html; charset=iso-8859-1",
+             "content-length: #{output.length}\r\n\r\n"].join("\r\n")
+
+  client.puts headers
+  client.puts output
+end
+
+puts ["Wrote this response:", headers, output, response].join("\n")
 client.close
 puts "\nResponse complete, exiting."
--- lib/server.rb
+++ lib/server.rb
@@ -2,44 +2,49 @@ require 'pry'
 require 'socket'
 
 class Server
-  attr_reader :client
-
   def initialize
-    @server              = TCPServer.new 9292
-    @server_requests     = 0
+    @server = TCPServer.new(9292)
+    @request_lines = []
+    @hello_counter = 0
   end
 
   def start
     @client = @server.accept
-    while @client = @server.accept
-      @server_requests += 1
-      @request_lines = []
-        while line = client.gets and !line.chomp.empty?
-          @request_lines << line.chomp
-        end
-      client.close
+    loop do
+      puts "Ready for Request"
+      while line = @client.gets and !line.chomp.empty?
+        @request_lines << line.chomp
+      end
+      response
     end
   end
 
   def response
-      response = ["VERB: #{@request_lines[0].split(" ")[0]}",
-                  "Path: #{@request_lines[0].split(" ")[1]}",
-                  "Protocol: #{@request_lines[0].split(" ")[2]}",
-                  "Host: #{@request_lines[1][6..14]}",
-                  "Port: #{@request_lines[1][-4..-1]}",
-                  "Origin: #{@request_lines[1][6..14]}",
-                  "#{@request_lines[6][0..7]}\n#{@request_lines[6][8..-1]}"].join("\n")
-  end
-
-  def output
-    output = "Hello World! (#{@server_requests - 1})\n{response}"
-  end
-
-  def headers
+    debug_information
+    output = "Hello World! (#{@hello_counter})\n#{debug_information}"
     headers = ["http/1.1 200 ok",
-               "date: #{Time.now.strftime('%a, %e %b %Y %H:%M:%S %z')}",
-               "server: ruby",
-               "content-type: text/html; charset=iso-8859-1",
-               "content-length: #{output.length}\r\n\r\n"].join("\r\n")
+              "date: #{Time.now.strftime('%a, %e %b %Y %H:%M:%S %z')}",
+              "server: ruby",
+              "content-type: text/html; charset=iso-8859-1",
+              "content-length: #{output.length}\r\n\r\n"].join("\r\n")
+    @client.puts headers
+    @client.puts output
+    puts ["Wrote this response:", headers, output].join("\n")
+    puts "\nResponse complete, exiting."
+    @hello_counter += 1
+    start
+  end
+
+  def debug_information
+    response = ["VERB: #{@request_lines[0].split(" ")[0]}",
+                "Path: #{@request_lines[0].split(" ")[1]}",
+                "Protocol: #{@request_lines[0].split(" ")[2]}",
+                "Host: #{@request_lines[1][6..14]}",
+                "Port: #{@request_lines[1][-4..-1]}",
+                "Origin: #{@request_lines[1][6..14]}",
+                "#{@request_lines[6][0..7]}\n#{@request_lines[6][8..-1]}"].join("\n")
   end
 end
+
+server = Server.new
+server.start
