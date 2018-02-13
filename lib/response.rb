4164df9aa20d123c36a1c7d2978cd1708a5648ca
alexcutschall <alexander.cutschall@gmail.com>
2018-02-13 08:11:50 -0700

    Added the headers into a seperate function and am calling it within each of my functions. Also cleaned up several functions so they no longer depict debug information.

--- lib/response.rb
+++ lib/response.rb
@@ -7,63 +7,56 @@ class Response
   def initialize(server)
     @server = server
     @hello_counter = 0
+    @output = nil
   end
 
-  def hello
-    debug_information
-    output = "Hello World! (#{@hello_counter})\n#{debug_information}"
+  def headers
     headers = ["http/1.1 200 ok",
                 "date: #{Time.now.strftime('%a, %e %b %Y %H:%M:%S %z')}",
                 "server: ruby",
                 "content-type: text/html; charset=iso-8859-1",
-                "content-length: #{output.length}\r\n\r\n"].join("\r\n")
+                "content-length: #{@output.length}\r\n\r\n"].join("\r\n")
+  end
+
+  def debug_information
+    verb     = "#{server.request_lines[0].split(" ")[0]}"
+    path     = "#{server.request_lines[0].split(" ")[1]}"
+    protocol = "#{server.request_lines[0].split(" ")[2]}"
+    host     = "#{server.request_lines[1][6..14]}"
+    port     = "#{server.request_lines[1][-4..-1]}"
+    origin   = "#{server.request_lines[1][6..14]}"
+
+    "<pre>\nVerb: #{verb}\nPath: #{path}\nProtocol: #{protocol}\nHost: #{host}\nPort: #{port}\nOrigin: #{origin}</pre>"
+  end
+
+  def hello
+    # debug_information
+    @output = "Hello World! (#{@hello_counter})\n#{debug_information}"
+    headers
     server.client.puts headers
-    server.client.puts output
-    puts ["Wrote this response:", headers, output].join("\n")
-    puts "\nResponse complete, exiting."
+    server.client.puts @output
     @hello_counter += 1
   end
 
   def debug_page
-    debug_information
-    output = "#{debug_information}"
-    headers = ["http/1.1 200 ok",
-                "date: #{Time.now.strftime('%a, %e %b %Y %H:%M:%S %z')}",
-                "server: ruby",
-                "content-type: text/html; charset=iso-8859-1",
-                "content-length: #{output.length}\r\n\r\n"].join("\r\n")
+    # debug_information
+    @output = "#{debug_information}"
+    headers
     server.client.puts headers
-    server.client.puts output
+    server.client.puts @output
   end
 
   def date_time
-    date = Time.now.strftime('%I:%M on %A, %B %d, %Y')
-    output = "#{date}\n#{debug_information}"
-    headers = ["http/1.1 200 ok",
-                "date: #{Time.now.strftime('%a, %e %b %Y %H:%M:%S %z')}",
-                "server: ruby",
-                "content-type: text/html; charset=iso-8859-1",
-                "content-length: #{output.length}\r\n\r\n"].join("\r\n")
+    @output = "#{Time.now.strftime('%I:%M on %A, %B %d, %Y')}"
+    headers
     server.client.puts headers
-    server.client.puts output
-  end
-
-  def debug_information
-    verb =  "#{server.request_lines[0].split(" ")[0]}"
-    path =  "#{server.request_lines[0].split(" ")[1]}"
-    protocol ="#{server.request_lines[0].split(" ")[2]}"
-    host =  "#{server.request_lines[1][6..14]}"
-    port =  "#{server.request_lines[1][-4..-1]}"
-    origin =  "#{server.request_lines[1][6..14]}"
-    accept = "#{server.request_lines[6][0..7]}\n#{server.request_lines[6][8..-1]}"
-
-    "<pre>\nVerb: #{verb}\nPath: #{path}\nProtocol: #{protocol}\n"
-    + "Host: #{host}\nPort: #{port}\nOrigin: #{origin}\n#{accept}</pre>"
+    server.client.puts @output
   end
 
   def word_search
-    word = debug_information.split("\n")[1].split("?")
+    word = server.request_lines[0].split(" ")[1].split("?")
     word_popped = word.shift
+
     file = File.read('/usr/share/dict/words').split("\n")
     result = word.map do |word|
       if file.include?(word)
@@ -71,20 +64,20 @@ class Response
       else
         "#{word} is not a known word"
       end
-    end
+    end.join("\n")
+
+    output = "#{result}"
+    # headers
+    # server.client.puts headers
+    server.client.puts output
   end
 
     def shutdown
       response = "Total Requests: #{server.total_requests}"
-      output = "#{response}\n#{debug_information}"
-      headers = ["http/1.1 200 ok",
-                  "date: #{Time.now.strftime('%a, %e %b %Y %H:%M:%S %z')}",
-                  "server: ruby",
-                  "content-type: text/html; charset=iso-8859-1",
-                  "content-length: #{output.length}\r\n\r\n"].join("\r\n")
-
+      @output = "#{response}\n#{debug_information}"
+      headers
       server.client.puts headers
-      server.client.puts output
+      server.client.puts @output
       server.server_loop = false
     end
 
--- test/server_test.rb
+++ test/server_test.rb
@@ -5,11 +5,21 @@ require 'faraday'
 class ServerTest < Minitest::Test
   def test_response_includes_GET
     request = Faraday.get('http://127.0.0.1:9292')
-    expected = 'GET'
+    assert request.body.include?('GET')
   end
 
   def test_class_prints_out_hello_world
-    skip
-    assert_equal response.body.include?("Hello, World!"), Faraday.get('http://127.0.0.1:9292')
+    request = Faraday.get('http://127.0.0.1:9292/hello')
+    assert request.body.include?('Hello World!')
+  end
+
+  def test_class_prints_out_date
+    request = Faraday.get('http://127.0.0.1:9292/datetime')
+    assert request.body.include?('2018')
+  end
+
+  def test_class_prints_out_word_search
+    request = Faraday.get('http://127.0.0.1:9292/word?apple?ban')
+    assert request.body.include?('known')
   end
 end
